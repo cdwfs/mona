e340f80d91a21d6d4cecd6b765c98568f05c75c9
Cort <github@dangerware.org>
2014-05-13 22:50:25 -0700

    Checked in the new algorithm parameters I've been testing with on my laptop.

--- evogpupso.h
+++ evogpupso.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#define kEvoIterationCount 4000       // Iteration count of main loop
-#define kEvoAlphaLimit 0.2            // alpha limit for images
+#define kEvoIterationCount 2000       // Iteration count of main loop
+#define kEvoAlphaLimit 0.1            // alpha limit for images
 #define kEvoAlphaOffset (kEvoAlphaLimit/2) //alpha offset
 #define kEvoCheckLimit 150 //number of times we have a non-improving value before we terminate
 #define kEvoOutputScale 2 //the factor by which we would like to scale up the image in the final render
-#define kEvoPsoParticleCount 30    // number of particles for PSO
+#define kEvoPsoParticleCount 64    // number of particles for PSO
 #define kEvoBlockDim 16   // grid width/height
 #define kEvoPsoIterationCount 1000 // maximum number of iterations at each PSO stage (overridden by checklimit)
 #define kEvoMaxTriangleCount 800 // number of triangles to render (max)
-#define kEvoPsoNeighborhoodSize 12 // Size of neighbhorhood (PSO topology)
+#define kEvoPsoNeighborhoodSize 24 // Size of neighbhorhood (PSO topology)
 
 //rgb class! initialized with float4's from our texture memory
 class rgba {
