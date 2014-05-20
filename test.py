0b4c26b2fd489e5638edb89ddfd46b6f57b02c59
Cort <github@dangerware.org>
2014-05-20 11:38:01 -0700

    test script now generates Gnuplot .p files for fitness and time graphs

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ import subprocess
 import sys
 
 inputs = [
-    "Hanspeter-Pfister_large.jpg",
-    "lena.jpg",
+#    "Hanspeter-Pfister_large.jpg",
+#    "lena.jpg",
     "lisa.jpg",
-    "Mondrian.jpg",
-    "monet-parliament.jpg",
-    "moonman.jpg",
-    "picasso_three_musicians_moma.jpg",
+#    "Mondrian.jpg",
+#    "monet-parliament.jpg",
+#    "moonman.jpg",
+#    "picasso_three_musicians_moma.jpg",
     "slisa.jpg",
 ]
 
@@ -21,7 +21,39 @@ if __name__ == "__main__":
     out_dir = "test-" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     os.mkdir(out_dir)
 
-    gnuplot_cmd = ""
+    gnuplot_fitness_file = path.join(out_dir, "fitness.p")
+    fitness_p = open(gnuplot_fitness_file, "w")
+    fitness_p.write("""\
+unset log                              # remove any log-scaling
+unset label                            # remove any previous labels
+set xtic auto                          # set xtics automatically
+set ytic auto                          # set ytics automatically
+set key outside right top
+set xlabel "Generation"
+set ylabel "PSNR (dB)"
+set xr [1:10000]
+set yr [0:40]
+set logscale x
+set terminal jpeg size 800,600 font "Arial,12"
+set output "graph-fitness.jpg"
+plot """)
+
+    gnuplot_time_file = path.join(out_dir, "time.p")
+    time_p = open(gnuplot_time_file, "w")
+    time_p.write("""\
+unset log                              # remove any log-scaling
+unset label                            # remove any previous labels
+set xtic auto                          # set xtics automatically
+set ytic auto                          # set ytics automatically
+set key outside right top
+set xlabel "Generation"
+set ylabel "Time (seconds)"
+set xr [1:10000]
+set yr [0:*]
+set logscale x
+set terminal jpeg size 800,600 font "Arial,12"
+set output "graph-time.jpg"
+plot """)
 
     for img in inputs:
         root,suffix = os.path.splitext(img)
@@ -42,6 +74,11 @@ if __name__ == "__main__":
                          "-particles", "16",
                          img
                          ])
-        gnuplot_cmd += "'%s' using 1:3 title '%s' with lines, " % (stats_file, root)
-    subprocess.call(["gnuplot", "-e",
-                     "load 'evogpupso.p'; set output '"+out_dir+"/graph-fitness.jpg'; plot " + gnuplot_cmd])
+        fitness_p.write("'%s' using 1:3 title '%s' with lines,\\\n" % (os.path.split(stats_file)[1], root))
+        time_p.write("'%s' using 1:2 title '%s' with lines,\\\n" % (os.path.split(stats_file)[1], root))
+
+    fitness_p.close()
+    time_p.close()
+    os.chdir(out_dir)
+    subprocess.call([ "gnuplot", "-e", "load '%s'" % os.path.split(gnuplot_fitness_file)[1] ])
+    subprocess.call([ "gnuplot", "-e", "load '%s'" % os.path.split(gnuplot_time_file)[1] ])
