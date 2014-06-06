6b061889230b0e94ae75e3284752baab065e264f
Cort Stratton <cort_stratton@naughtydog.com>
2014-06-05 18:51:52 -0700

    xmona: Moved CUDA_CHECK() into zombolite.h

--- xmona/main.cpp
+++ xmona/main.cpp
@@ -1,36 +1,9 @@
+#include "zombolite.h"
 #include "xmona.h"
 
 #include <QtWidgets/QApplication>
 
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <cuda_runtime_api.h>
-
 #include <cassert>
-
-// Use CUDA_CHECK() in host code to validate the result of CUDA functions that return an error.
-#define ENABLE_CUDA_CHECK 1 // disable if you want CUDA_CHECK() to compile away
-#if ENABLE_CUDA_CHECK
-#define CUDA_CHECK(val) \
-	do{ \
-	cudaError err = (val); \
-	if (err != cudaSuccess) { \
-	char finalMsg[256]; \
-	_snprintf_s(finalMsg, 255, "CUDA error in %s:\n%d:\t%s\n%s\n", __FILE__, __LINE__, #val, cudaGetErrorString(err)); \
-	finalMsg[255] = 0; \
-	OutputDebugStringA(finalMsg); \
-	if (IsDebuggerPresent()) \
-	__debugbreak(); \
-				else \
-				assert(err == cudaSuccess); \
-	} \
-	__pragma(warning(push)) \
-	__pragma(warning(disable:4127)) /* constant conditional */ \
-	} while(0) \
-	__pragma(warning(pop))
-#else
-#define CUDA_CHECK(val) (val)
-#endif
 
 int main(int argc, char *argv[])
 {
--- xmona/zombolite.h
+++ xmona/zombolite.h
@@ -2,9 +2,38 @@
 
 #include "gl_core_3_3.h"
 
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <cuda_gl_interop.h>
+
 #include <cassert>
 #include <cstdint>
 #include <string>
+
+// Use CUDA_CHECK() in host code to validate the result of CUDA functions that return an error.
+#define ENABLE_CUDA_CHECK 1 // disable if you want CUDA_CHECK() to compile away
+#if ENABLE_CUDA_CHECK
+#define CUDA_CHECK(val) \
+	do{ \
+		cudaError err = (val); \
+		if (err != cudaSuccess) { \
+			char finalMsg[256]; \
+			_snprintf_s(finalMsg, 255, "CUDA error in %s:\n%d:\t%s\n%s\n", __FILE__, __LINE__, #val, cudaGetErrorString(err)); \
+			finalMsg[255] = 0; \
+			OutputDebugStringA(finalMsg); \
+			if (IsDebuggerPresent()) \
+				__debugbreak(); \
+			else \
+				assert(err == cudaSuccess); \
+		} \
+		__pragma(warning(push)) \
+		__pragma(warning(disable:4127)) /* constant conditional */ \
+	} while(0) \
+	__pragma(warning(pop))
+#else
+#define CUDA_CHECK(val) (val)
+#endif
 
 // Custom assert macro that prints a formatted error message and breaks immediately from the calling code
 // (instead of instead the code to _wassert)
